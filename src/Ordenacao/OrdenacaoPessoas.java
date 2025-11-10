44b9e05d1400f82975f2a2b5aa515ca1b5b0d0c4
marcusteixeirabr <marcusteixeirabr@gmail.com>
2025-11-10 14:26:20 -0300

    Adiciona package Ordencao e inclui projeto Ordenação de Pessoas, com duas classe OrdenacaoPessoas e Pessoa

---
+++ src/Ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,53 @@
+package Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, float altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+        System.out.println("Registro " + nome + " adicionado com sucesso!");
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoaPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
+    }
+
+    public static void main(String[] args) {
+
+        OrdenacaoPessoas l1 = new OrdenacaoPessoas();
+
+        l1.adicionarPessoa("Maria", 23, 1.76f);
+        l1.adicionarPessoa("João", 32, 1.56f);
+        l1.adicionarPessoa("Carla", 12, 1.46f);
+        l1.adicionarPessoa("Farias", 55, 1.87f);
+        l1.adicionarPessoa("Tereza", 43, 1.56f);
+        l1.adicionarPessoa("Marcelo", 5, 1.05f);
+        l1.adicionarPessoa("Flávia", 33, 1.42f);
+        l1.adicionarPessoa("Pedro", 35, 1.91f);
+        l1.adicionarPessoa("Ana", 65, 1.67f);
+        l1.adicionarPessoa("José", 76, 1.83f);
+
+        System.out.println(l1.ordenarPorIdade() + "\n");
+        System.out.println(l1.ordenarPorAltura());
+
+
+
+    }
+
+}
---
+++ src/Ordenacao/Pessoa.java
@@ -0,0 +1,45 @@
+package Ordenacao;
+
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa>{
+    private String nome;
+    private int idade;
+    private float altura;
+
+    public Pessoa(String nome, int idade, float altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    @Override
+    public int compareTo(Pessoa p) {
+        return Integer.compare(idade, p.getIdade());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public float getAltura() {
+        return altura;
+    }
+
+    @Override
+    public String toString() {
+        return "nome = '" + nome + "'" +
+             ", idade = " + idade +
+             ", altura = " + altura +
+             "\n";
+    }
+}
+
+class ComparatorPorAltura implements Comparator<Pessoa> {
+    @Override
+    public int compare(Pessoa p1, Pessoa p2) {
+        return Float.compare(p1.getAltura(), p2.getAltura());
+    }
+}
