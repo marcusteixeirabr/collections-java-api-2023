aed5147904ae3de27433974f0265ceea80e58827
marcusteixeirabr <marcusteixeirabr@gmail.com>
2025-11-17 20:27:37 -0300

    Finaliza as Classes Tarefa e ListaTarefas

--- src/Set/OperacoesBasicas/ListaTarefas.java
+++ src/Set/OperacoesBasicas/ListaTarefas.java
@@ -12,20 +12,126 @@ public class ListaTarefas {
 
     public void adicionarTarefa(String descricao) {
         tarefas.add(new Tarefa(descricao));
+        System.out.println("Tarefa '" + descricao + "' adicionada à lista.");
+        System.out.println("-".repeat(60));
     }
 
+    public void removerTarefa(String descricao) {
+        tarefas.removeIf(tarefa -> tarefa.getDescricao().equals(descricao));
+        /*Tarefa tarefaParaRemover = null;
+        for (Tarefa t : tarefas) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefaParaRemover = t;
+                break;
+            }
+        }
+        tarefas.remove(tarefaParaRemover);*/
+    System.out.println("Tarefa '" + descricao + "' removida da lista.");
+    System.out.println("-".repeat(60));
+    }
 
+    public void exibirTarefas() {
+        if (tarefas.isEmpty()) {
+            System.out.println("Não há tarefas na lista!");
+            return;
+        }
+        for (Tarefa t : tarefas) {
+            System.out.println(t.getDescricao() + " - " + (t.isConcluida() ? "Concluída" : "Pendente"));
+        }
+        System.out.println("-".repeat(60));
+    }
 
+    public int contarTarefas() {
+        return tarefas.size();
+    }
 
+    public void obterTarefasConcluidas() {
+        int count = 0;
+        System.out.println("Tarefas Concluídas:");   
+        for (Tarefa t : tarefas) {
+            if (t.isConcluida()) {
+                count++;
+                System.out.println(t.getDescricao());
+            }
+        }
+        if (count == 0) {
+            System.out.println("Nenhuma tarefa concluída!");
+        }
+        System.out.println("-".repeat(60));
+    }
 
+    public void obterTarefasPendentes() {
+        int count = 0;
+        System.out.println("Tarefas Pendentes:");
+        for (Tarefa t : tarefas) {
+            if (!t.isConcluida()) {
+                count++;
+                System.out.println(t.getDescricao());
+            }
+        }
+        if (count == 0) {
+            System.out.println("Nenhuma tarefa pendente!");
+        }
+        System.out.println("-".repeat(60));
+    }
 
+    public void marcarTarefaConcluida(String descricao) {
+        for (Tarefa t : tarefas) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                t.setConcluida(true);
+                System.out.println("Tarefa '" + descricao + "' marcada como concluída.");
+                break;
+            }
+        }
+        System.out.println("-".repeat(60));
+    }
 
+    public void marcarTarefaPendente(String descricao) {
+        for (Tarefa t : tarefas) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                t.setConcluida(false);
+                System.out.println("Tarefa '" + descricao + "' marcada como pendente.");
+                break;
+            }
+        }
+        System.out.println("-".repeat(60));
+    }
 
-
-
+    public void limparListaTarefas() {
+        tarefas.clear();
+        System.out.println("Lista de tarefas limpa comsucesso!");
+        System.out.println("-".repeat(60));
+    }
 
     public static void main(String[] args) {
         
+        System.out.println("-".repeat(60));
+
+        ListaTarefas toDoList = new ListaTarefas();
+
+        toDoList.adicionarTarefa("Estudar Java Collections");
+        toDoList.adicionarTarefa("Fazer exercícios de programação");
+        toDoList.adicionarTarefa("Ler documentação da API Java");
+
+        toDoList.exibirTarefas();
+
+        toDoList.marcarTarefaConcluida("Estudar Java Collections");
+        toDoList.removerTarefa("Ler documentação da API Java");
+
+        toDoList.exibirTarefas();
+
+        toDoList.adicionarTarefa("Ler documentação Copilot");
+        toDoList.adicionarTarefa("Fazer manutenção Fedora 43");
+        toDoList.adicionarTarefa("Fazer manutenção Fedora 43");
+
+        toDoList.marcarTarefaPendente("Estudar Java Collections");
+        toDoList.marcarTarefaConcluida("Ler documentação Copilot");
+
+        toDoList.obterTarefasConcluidas();
+        toDoList.obterTarefasPendentes();
+
+
+
     }
 
 }
--- src/Set/OperacoesBasicas/Tarefa.java
+++ src/Set/OperacoesBasicas/Tarefa.java
@@ -25,4 +25,31 @@ public class Tarefa {
         this.concluida = concluida;
     }
 
+    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tarefa other = (Tarefa) obj;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        return true;
+    }
+      
 }
