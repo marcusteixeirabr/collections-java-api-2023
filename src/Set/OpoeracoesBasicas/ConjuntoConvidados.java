5dea81bcf1c3a5be4786542adb4b2b71799c6ae3
marcusteixeirabr <marcusteixeirabr@gmail.com>
2025-11-17 16:57:57 -0300

    Retira imports desnecessários de ConjuntoConvidados.java.

--- src/Set/OpoeracoesBasicas/ConjuntoConvidados.java
+++ src/Set/OpoeracoesBasicas/ConjuntoConvidados.java
@@ -1,9 +1,7 @@
 package Set.OpoeracoesBasicas;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.Callable;
 
 public class ConjuntoConvidados {
     Set<Convidado> convidados;
