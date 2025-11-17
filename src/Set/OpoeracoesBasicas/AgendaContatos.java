8ad038554a22a98f8c6203cc3089fda5852ef304
marcusteixeirabr <marcusteixeirabr@gmail.com>
2025-11-17 12:22:07 -0300

    Adiciona pastas List e Set com arquivos e exercícios do módulo conhecendo collections java

--- src/Set/OpoeracoesBasicas/AgendaContatos.java
+++ src/Set/OpoeracoesBasicas/AgendaContatos.java
@@ -1,4 +1,76 @@
 package Set.OpoeracoesBasicas;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AgendaContatos {
+    Set<Contato> contatosSet;
+
+    public AgendaContatos() {
+        contatosSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int telefone) {
+        contatosSet.add(new Contato(nome, telefone));
+    }
+
+    public void exibirContatos() {
+        if (contatosSet.isEmpty()) {
+            System.out.println("A agenda de contatos está vazia!");
+            return;
+        }
+        System.out.println(contatosSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatoPorNome = new HashSet<>();
+        for (Contato c : contatosSet) {
+            if (c.getNome().contains(nome)) {
+                contatoPorNome.add(c);
+            }
+        }
+        return contatoPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        for (Contato c : contatosSet) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                c.AtualizaTelefone(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+
+        AgendaContatos agenda = new AgendaContatos();
+
+        agenda.adicionarContato("Marcus", 999911325);
+        agenda.adicionarContato("Joana", 945911377);
+        agenda.adicionarContato("Abílio", 939910025);
+        agenda.adicionarContato("Carol", 995915355);
+        agenda.adicionarContato("José", 979911026);
+        agenda.adicionarContato("Gabi", 989811424);
+        agenda.adicionarContato("Gabi", 33982421);
+        agenda.adicionarContato("Gabi Pet", 33982421);
+        agenda.adicionarContato("Gabi Java", 99982499);
+
+
+
+        agenda.exibirContatos();
+
+        System.out.println();
+        System.out.println(agenda.pesquisarPorNome("Gabi"));
+        System.out.println();
+
+        agenda.atualizarNumeroContato("Gabi", 912341234);
+
+        agenda.exibirContatos();
+
+    }
 }
+
+
--- src/Set/OpoeracoesBasicas/Contato.java
+++ src/Set/OpoeracoesBasicas/Contato.java
@@ -1,4 +1,45 @@
 package Set.OpoeracoesBasicas;
 
+import java.util.Objects;
+
 public class Contato {
+    private String nome;
+    private int telefone;
+
+    public Contato(String nome, int telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object c) {
+        if (c == null || getClass() != c.getClass()) return false;
+        Contato contato = (Contato) c;
+        return getNome().equalsIgnoreCase(contato.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getNome());
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void AtualizaTelefone(int novoNumero) {
+        telefone = novoNumero;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" +
+                "nome='" + nome + '\'' +
+                ", telefone=" + telefone +
+                '}';
+    }
 }
